dfa3ef01a404f45fd10e4743f2071b9b3c1c6009
Anish Arora <anish.arora@anishar-ltm4rr8.internal.salesforce.com>
2025-07-22 00:11:32 +0530

    another commit to test pipeline

--- parsePR.js
+++ parsePR.js
@@ -2,7 +2,6 @@ const fs = require('fs');
 const readline = require('readline');
 
 async function extractTests() {
-    // Default: run all tests
     const testsFile = __dirname + '/testsToRun.txt';
     await fs.promises.writeFile(testsFile, 'all');
 
@@ -12,10 +11,9 @@ async function extractTests() {
     });
 
     for await (const line of lines) {
-        // Detect pattern: Apex::[...tests...]::Apex
-        if (line.includes('Apex::[') && line.includes(']::Apex')) {
-            let tests = line.substring(8, line.length - 7); // Extract contents inside brackets
-            let spaceSeparatedTests = tests.replace(/,/g, ' '); // Replace commas with spaces
+        const match = line.match(/Apex::\[(.*?)\]::Apex/);
+        if (match && match[1]) {
+            const spaceSeparatedTests = match[1].replace(/,/g, ' ');
             await fs.promises.writeFile(testsFile, spaceSeparatedTests + '\n');
         }
     }
