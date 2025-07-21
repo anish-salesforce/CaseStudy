99a8a723259785e82425fe9501979088a64811a9
Anish Arora <anish.arora@anishar-ltm4rr8.internal.salesforce.com>
2025-07-21 22:26:12 +0530

    another commit to test pipeline

--- parsePR.js
+++ parsePR.js
@@ -1,24 +1,22 @@
 const fs = require('fs');
-const readline = require('readline')
+const readline = require('readline');
 
-async function extractTests(){
-
-    //by default we specify that all tests should run
-    let testsFile = __dirname+'/testsToRun.txt';
-    await fs.promises.writeFile(testsFile,'all');
+async function extractTests() {
+    // Default: run all tests
+    const testsFile = __dirname + '/testsToRun.txt';
+    await fs.promises.writeFile(testsFile, 'all');
 
     const lines = readline.createInterface({
-        input: fs.createReadStream(__dirname+'/pr_body.txt'),
+        input: fs.createReadStream(__dirname + '/pr_body.txt'),
         crlfDelay: Infinity
     });
 
     for await (const line of lines) {
-        //special delimeter for apex tests
-        if(line.includes('Apex::[') && line.includes(']::Apex')){
-
-            let tests = line.substring(8,line.length-7);
-            await fs.promises.writeFile(testsFile,tests);
-            await fs.promises.appendFile(testsFile,'\n');
+        // Detect pattern: Apex::[...tests...]::Apex
+        if (line.includes('Apex::[') && line.includes(']::Apex')) {
+            let tests = line.substring(8, line.length - 7); // Extract contents inside brackets
+            let spaceSeparatedTests = tests.replace(/,/g, ' '); // Replace commas with spaces
+            await fs.promises.writeFile(testsFile, spaceSeparatedTests + '\n');
         }
     }
 }
